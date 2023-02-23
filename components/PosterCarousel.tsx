10c461d47d44cd0a0cd58c5ab1df0ff78ac2e4d9
Tewson Seeoun <tewson.seeoun@gmail.com>
2023-02-23 21:28:48 +0000

    Add poster name

--- components/PosterCarousel.tsx
+++ components/PosterCarousel.tsx
@@ -1,24 +1,53 @@
 import React, {useEffect, useMemo, useState} from 'react';
-import {Image, Linking, View, StyleSheet} from 'react-native';
+import {Image, Linking, View, StyleSheet, Text} from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 import {TouchableHighlight} from 'react-native-gesture-handler';
+import {useTheme} from '@react-navigation/native';
 
 import {StrapiEntryResponse, StrapiEntryListResponse} from '../utils/strapi';
 
 interface Poster {
+  name?: string;
   image: StrapiEntryResponse<{url: string}>;
   url?: string;
 }
 
 function PosterCarouselItem({item, height}: {item: Poster; height: number}) {
+  const {colors} = useTheme();
+  const styles = useMemo(
+    () =>
+      StyleSheet.create({
+        posterNameContainer: {
+          position: 'absolute',
+          left: 8,
+          bottom: 8,
+          backgroundColor: colors.background,
+          padding: 4,
+        },
+        posterName: {
+          fontSize: 24,
+          color: colors.text,
+          textDecorationLine: 'underline',
+        },
+      }),
+    [colors.background, colors.text],
+  );
+
   return (
     <View key={item.image.data.attributes.url}>
       <TouchableHighlight
         onPress={item.url ? () => Linking.openURL(item.url!) : undefined}>
-        <Image
-          style={{width: height, height: height}}
-          source={{uri: item.image.data.attributes.url}}
-        />
+        <View>
+          <Image
+            style={{width: height, height: height}}
+            source={{uri: item.image.data.attributes.url}}
+          />
+          {item.name ? (
+            <View style={styles.posterNameContainer}>
+              <Text style={styles.posterName}>{item.name}</Text>
+            </View>
+          ) : null}
+        </View>
       </TouchableHighlight>
     </View>
   );
